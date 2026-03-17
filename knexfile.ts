6389a814a8755faa9d754929a83a1c776bd67b7a
Rob Gietema <rob.gietema@gmail.com>
2026-03-17 09:46:13 +0200

    Get knex config from helper.

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,12 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { config } from './config.js';
+import config from './src/helpers/config/config';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const knexSettings = {
   client: 'pg',
-  connection: config.connection,
+  connection: config.settings.connection,
   pool: {
     min: 2,
     max: 10,
